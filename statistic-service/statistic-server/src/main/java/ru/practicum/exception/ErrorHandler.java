052dac35ad4120d5a8ad634db114b15ae5074e37
ymolyavkin <mat2017u@yandex.ru>
2023-08-28 19:13:15 +0300

    refactor : added ErrorHandler class

--- statistic-service/statistic-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ statistic-service/statistic-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -2,10 +2,12 @@ package ru.practicum.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ValidationException;
 import java.util.Map;
 
 @Slf4j
@@ -17,5 +19,16 @@ public class ErrorHandler {
         log.debug("Ошибка IllegalArgumentException. Статус ошибки 400 Bad Request{}", e.getMessage(), e);
         return Map.of("error", e.getMessage());
     }
-
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMissingParams(MissingServletRequestParameterException e) {
+        log.debug("Ошибка MissingServletRequestParameterException. Отсутствует обязательный параметр запросаСтатус ошибки 400 Bad Request{}", e.getMessage(), e);
+        return Map.of("error", e.getMessage());
+    }
+    @ExceptionHandler({ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(final RuntimeException e) {
+        log.debug("Ошибка валидации. Получен статус 400 BAD_REQUEST {}", e.getMessage(), e);
+        return Map.of("error", e.getMessage());
+    }
 }
