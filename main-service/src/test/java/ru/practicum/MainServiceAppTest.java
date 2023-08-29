e88c91492f334d39ad5e4b62cd099e260e91ad77
ymolyavkin <mat2017u@yandex.ru>
2023-08-29 16:50:52 +0300

    feat : added in pom.xml

--- main-service/src/test/java/ru/practicum/MainServiceAppTest.java
+++ main-service/src/test/java/ru/practicum/MainServiceAppTest.java
@@ -4,13 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class AppTest extends TestCase {
+public class MainServiceAppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest(String testName) {
+    public MainServiceAppTest(String testName) {
         super(testName);
     }
 
@@ -18,7 +18,7 @@ public class AppTest extends TestCase {
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(AppTest.class);
+        return new TestSuite(MainServiceAppTest.class);
     }
 
     /**
