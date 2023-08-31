72bd90714c5f23175be83f8c457276c1233fac71
ymolyavkin <mat2017u@yandex.ru>
2023-08-31 13:45:22 +0500

    test : added HitControllerTest blank class

---
+++ statistic-service/statistic-server/src/test/java/ru/practicum/controller/HitControllerTest.java
@@ -0,0 +1,12 @@
+package ru.practicum.controller;
+
+import junit.framework.TestCase;
+
+public class HitControllerTest extends TestCase {
+
+    public void testGetViewStatistics() {
+    }
+
+    public void testAddHit() {
+    }
+}
