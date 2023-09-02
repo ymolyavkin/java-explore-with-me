6897169a276b7ac1c934c63395c55ffa3721cc47
ymolyavkin <mat2017u@yandex.ru>
2023-09-02 18:23:36 +0300

    feat : deleted annotation @Validated from UserController class

--- main-service/src/main/java/ru/practicum/ewm/controller/publicapi/UserController.java
+++ main-service/src/main/java/ru/practicum/ewm/controller/publicapi/UserController.java
@@ -5,20 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.dto.user.UserDto;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/admin/users")
 @Slf4j
-@Validated
+//@Validated
 @RequiredArgsConstructor
 public class UserController {
-    @GetMapping
+    /*@GetMapping
     public ResponseEntity<Object> test(HttpServletRequest request) {
         log.info("Получен тестовый запрос на эндпоинт /users GET");
 
@@ -27,11 +25,18 @@ public class UserController {
 
         return null;
     }
+*/
+    @GetMapping
+    public ResponseEntity<Object> getUserInfo() {
+        log.info("Получен запрос на получение информации о пользователях");
+
+        return null;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Object> addNewUser(@Valid @RequestBody UserDto userDto) {
-        log.info("Получен тестовый запрос на эндпоинт /users POST");
+        log.info("Получен запрос на добавление нового пользователя");
 
         return null;
     }
