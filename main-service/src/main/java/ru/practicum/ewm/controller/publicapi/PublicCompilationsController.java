9e5e21a4f5152d592ee8434d1e1de4b0e72fa43d
ymolyavkin <mat2017u@yandex.ru>
2023-09-03 09:37:17 +0300

    feat : added PublicCompilationsController class

---
+++ main-service/src/main/java/ru/practicum/ewm/controller/publicapi/PublicCompilationsController.java
@@ -0,0 +1,50 @@
+package ru.practicum.ewm.controller.publicapi;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.dto.compilation.CompilationDto;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+import static ru.practicum.util.Constants.PAGE_DEFAULT_FROM;
+import static ru.practicum.util.Constants.PAGE_DEFAULT_SIZE;
+
+@RestController
+@RequestMapping(value = "/compilations")
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+public class PublicCompilationsController {
+   /* @GetMapping
+    public ResponseEntity<Object> test(HttpServletRequest request) {
+        log.info("Получен тестовый запрос");
+        System.out.println(request.getLocalAddr());
+        System.out.println(request.getRemoteAddr());
+        log.info("client ip: {}", request.getRemoteAddr());
+        log.info("endpoint path: {}", request.getRequestURI());
+
+        return null;
+    }*/
+
+    @GetMapping
+    public List<CompilationDto> getAllCompilations(@RequestParam(defaultValue = PAGE_DEFAULT_FROM) @PositiveOrZero int from,
+   // public ResponseEntity<Page<CompilationDto>> getAllCompilations(@Valid @RequestParam(defaultValue = PAGE_DEFAULT_FROM) @PositiveOrZero int from,
+                                                   @RequestParam(defaultValue = PAGE_DEFAULT_SIZE) @Positive int size,
+                                                   @RequestParam Boolean pinned) {
+        log.info("Получен запрос на получение подборок событий");
+
+
+        return null;
+    }
+    @GetMapping("/{id}")
+    public CompilationDto getCompilationById(@PathVariable Long id) {
+        log.info("Получен запрос на получение события с id {}", id);
+        //return compilationService.getCompilationById(compId);
+        return  null;
+    }
+
+}
--- statistic-service/statistic-dto/src/main/java/ru/practicum/util/Constants.java
+++ statistic-service/statistic-dto/src/main/java/ru/practicum/util/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String MESSAGE_VALIDATION_SIZE = "Размер поля не удовлетворяет ограничениям";
     public static final String MESSAGE_VALIDATION_POSITIVE = "Ожидается неотрицательное значение";
     public static final String MESSAGE_VALIDATION_START_AFTER_END = "Начало временного интервала позже его окончания";
+    public static final String PAGE_DEFAULT_FROM = "0";
+    public static final String PAGE_DEFAULT_SIZE = "10";
 }
