eb1cef768f0715e08c858d7ad2d19a415d1a139c
ymolyavkin <mat2017u@yandex.ru>
2023-09-04 18:08:17 +0500

    fix : fixed StatsClient class

--- statistic-service/statistic-client/src/main/java/ru/practicum/client/StatsClient.java
+++ statistic-service/statistic-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -2,7 +2,6 @@ package ru.practicum.client;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,15 +9,16 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 @Service
 public class StatsClient {
-    @Value("${stats.uri}")
-    private String uri;
+    private final String uri="http://localhost:8080";
     private final RestTemplate restTemplate;
-    public  String saveStats(String item){
-        ResponseEntity<String> response = restTemplate.postForEntity(uri+"/stats", item, String.class);
+
+    public String saveStats(String item) {
+        ResponseEntity<String> response = restTemplate.postForEntity(uri + "/stats", item, String.class);
         return response.getBody();
     }
-    public  Integer getCount(){
-        ResponseEntity<Integer> response = restTemplate.getForEntity(uri+"/", Integer.class);
+
+    public Integer getCount() {
+        ResponseEntity<Integer> response = restTemplate.getForEntity(uri + "/", Integer.class);
         return response.getBody();
     }
 }
