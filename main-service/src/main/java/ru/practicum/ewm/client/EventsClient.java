261c80848a4793e798f472e8713436773220d114
ymolyavkin <mat2017u@yandex.ru>
2023-09-10 20:47:16 +0300

    style : style

--- main-service/src/main/java/ru/practicum/ewm/client/EventsClient.java
+++ main-service/src/main/java/ru/practicum/ewm/client/EventsClient.java
@@ -1,4 +1,5 @@
 package ru.practicum.ewm.client;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
