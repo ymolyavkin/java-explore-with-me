48d3ac09d701043dfd3fad8b047b7f177915bd0d
ymolyavkin <mat2017u@yandex.ru>
2023-09-14 18:15:58 +0300

    refactor : edited ParticipationRequestDto class

--- main-service/src/main/java/ru/practicum/ewm/dto/request/ParticipationRequestDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/request/ParticipationRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,14 @@ import ru.practicum.ewm.enums.RequestStatus;
 
 import java.time.LocalDateTime;
 
+import static ru.practicum.util.Constants.DATE_TIME_PATTERN;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParticipationRequestDto {
     private Long id;
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
     private LocalDateTime created;
     private Long event;
     private Long requester;
