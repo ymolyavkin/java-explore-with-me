3ba5ade16ce3802c035c0a9213788da82ab630e7
ymolyavkin <mat2017u@yandex.ru>
2023-09-15 20:41:51 +0300

    refactor : added groups for validation

--- main-service/src/main/java/ru/practicum/ewm/dto/compilation/NewCompilationDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/compilation/NewCompilationDto.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.dto.compilation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.validator.Marker;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -17,7 +18,8 @@ import static ru.practicum.util.Constants.MESSAGE_VALIDATION_SIZE;
 public class NewCompilationDto {
     private List<Long> events;
     private Boolean pinned;
-    @NotBlank(message = MESSAGE_VALIDATION_NOT_BLANK)
-    @Size(min = 1, max = 50, message = MESSAGE_VALIDATION_SIZE)
+
+    @NotBlank(groups = {Marker.OnCreate.class, Marker.OnUpdate.class}, message = MESSAGE_VALIDATION_NOT_BLANK)
+    @Size(groups = {Marker.OnCreate.class, Marker.OnUpdate.class}, min = 1, max = 50, message = MESSAGE_VALIDATION_SIZE)
     private String title;
 }
