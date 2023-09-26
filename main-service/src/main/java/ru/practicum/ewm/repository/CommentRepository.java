e7faf58d498e3084325801a7d78c086facbb636c
ymolyavkin <mat2017u@yandex.ru>
2023-09-26 14:17:55 +0500

    refactor : refactor query

--- main-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
@@ -1,20 +1,20 @@
 package ru.practicum.ewm.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.ewm.entity.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    /* @Query("select c from Comment c " +
-             "where :text is null or (lower(c.text) like lower(concat('%', :text, '%'))) " +
-             "order by c.created"
-     )*/
+    @Query("select c from Comment c " +
+           "where :text is null or (lower(c.text) like lower(concat('%', :text, '%'))) " +
+           "order by c.created")
     Page<Comment> findAllByTextOrderByCreated(String text, Pageable pageable);
 
     List<Comment> findAllByEventIdOrderByCreated(Long eventId, Pageable pageable);
 
     List<Comment> findAllByAuthorIdOrderByCreated(Long authorId, Pageable pageable);
-
 }
