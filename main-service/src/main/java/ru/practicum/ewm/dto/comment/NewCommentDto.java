c51a858ac530bdb6dbd0dd1692a010e8df0646b2
ymolyavkin <mat2017u@yandex.ru>
2023-09-26 17:25:03 +0300

    feat : added feature.json

--- main-service/src/main/java/ru/practicum/ewm/dto/comment/NewCommentDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/comment/NewCommentDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practicum.validator.Marker;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
